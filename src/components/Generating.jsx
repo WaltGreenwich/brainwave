23c64d084ee672bc6b7b195451a5b023237fecde
WGreenwichChaca <walter.greenwich@gmail.com>
2024-09-30 03:22:54 -0500

    Implementando el componente Notification

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -1,6 +1,6 @@
 import { loading } from "../assets";
 
-const Generating = () => {
+const Generating = ({ className }) => {
   return (
     <div
       className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${
