04ff1e2917952d183621d828f3b159c218ddfc82
WGreenwichChaca <walter.greenwich@gmail.com>
2024-10-03 03:35:05 -0500

    Implemendando la seccion Benefits

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chatea de manera más inteligente, no más complicada, con Brainwave"
+        />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
