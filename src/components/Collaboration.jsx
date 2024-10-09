add7e565f010a4158c1a48951d2e5819c931e9d4
WGreenwichChaca <walter.greenwich@gmail.com>
2024-10-09 00:41:29 -0500

    Dando formato a Collaboration Component

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Section from "./Section";
 
 const Collaboration = () => {
-  return <div>Collaboration</div>;
+  return (
+    <Section crosses>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            App de Chat con IA para una colaboración sin límites
+          </h2>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Collaboration;
