8ec7e409205a4cbf1c4e1059e73002117e370d57
WGreenwichChaca <walter.greenwich@gmail.com>
2024-10-13 12:15:47 -0500

    Creando el componente Roadmap

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ Benefits;
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
